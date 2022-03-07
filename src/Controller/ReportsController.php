e2644efb00ac0609a1395f545df461c844123db6
Carlos Maradiaga <cmaradiaga715@gmail.com>
2022-03-06 18:33:22 -0600

    data in table

--- src/Controller/ReportsController.php
+++ src/Controller/ReportsController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use MongoDB\Client;
+use MongoDB\BSON\Regex;
 
 
 class ReportsController extends AbstractController
@@ -18,15 +19,68 @@ class ReportsController extends AbstractController
     {
         $conex = new Client('mongodb://localhost:27017');
         $collection = $conex->cnx->user;
-        $data = $collection->find()->toArray();
-        $arrayData = json_decode(json_encode($data));
 
-        // var_dump($array[3]);
+        //agregations
+        $isActive = [
+            '$match' => ['status' => true]
+        ];
+
+        $list = [
+            '$lookup' => [
+                "from" => "statics",
+                "localField" => "_id",
+                "foreignField" => "userId",
+                "as" => "statics"
+            ]
+        ];
+
+        $un =  [
+            '$unwind' => '$statics'
+
+        ];
+
+        $group =  [
+            '$group' => [
+                '_id' => '$accountId',
+                'name' => [
+                    '$first' => '$name'
+                ],
+                'lastname' => [
+                    '$first' => '$lastname'
+                ],
+                'clicks' => [
+                    '$sum' => '$statics.clicks'
+                ],
+                'impressions' => [
+                    '$sum' => '$statics.impressions'
+                ],
+                'spend' => [
+                    '$sum' => '$statics.spend'
+                ]
+            ]
+        ];
+
+        $add = [
+            '$addFields' => [
+                'costPerClick' => [
+                    '$divide' => ['$spend', '$clicks'] 
+                ]     
+            ]
+        ];
+
+        $sort = [
+            '$sort' => [
+                '_id' => 1
+            ]
+        ];
+
+        //data
+        $data = $collection->aggregate([$isActive, $list, $un, $group, $add, $sort])->toArray();
+        $arrayData = json_decode(json_encode($data));
 
         return $this->render('reports/index.html.twig', [
             'controller_name' => 'ReportsController',
             'data' => $arrayData
-
         ]);
     }
 }
